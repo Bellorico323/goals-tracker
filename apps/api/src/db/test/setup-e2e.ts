48a4d19a38ef7e3a3b4460f56032df19e3144c1f
Bellorico323 <mabo.orico@gmail.com>
2024-08-06 22:29:10 -0300

    setup file

--- apps/api/src/db/test/setup-e2e.ts
+++ apps/api/src/db/test/setup-e2e.ts
@@ -1,43 +1,36 @@
 import 'dotenv/config'
 
-import { randomUUID } from 'crypto'
+import { randomUUID } from 'node:crypto'
+
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import postgres from 'postgres'
-import { Environment } from 'vitest/environments'
 
-function generateDatabaseURL(schema: string) {
+function generateDatabaseURL() {
   if (!process.env.DATABASE_URL) {
     throw new Error('Please provide a DATABASE_URL environment variable.')
   }
 
   const url = new URL(process.env.DATABASE_URL)
-  url.searchParams.set('schema', schema)
 
   return url.toString()
 }
 
-export default <Environment>{
-  name: 'drizzle',
-  transformMode: 'ssr',
-  async setup() {
-    const schema = randomUUID()
-    const databaseURL = generateDatabaseURL(schema)
+const schema = randomUUID()
+let connection: postgres.Sql<Record<string, never>>
 
-    process.env.DATABASE_URL = databaseURL
+beforeAll(async () => {
+  const databaseURL = generateDatabaseURL()
 
-    const connection = postgres(databaseURL, { max: 1 })
-    await connection`CREATE SCHEMA IF NOT EXISTS ${schema}`
-    await connection`SET search_path TO ${schema}`
+  connection = postgres(databaseURL, { max: 1 })
+  await connection.unsafe(`CREATE SCHEMA IF NOT EXISTS "${schema}"`)
+  await connection.unsafe(`SET search_path TO "${schema}"`)
 
-    const db = drizzle(connection)
-    await migrate(db, { migrationsFolder: 'drizzle' })
+  const db = drizzle(connection)
+  await migrate(db, { migrationsFolder: 'drizzle' })
+})
 
-    return {
-      async teardown() {
-        await connection`DROP SCHEMA IF EXISTS ${schema} CASCADE`
-        await connection.end()
-      },
-    }
-  },
-}
+afterAll(async () => {
+  // await connection.unsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+  await connection.end()
+})
