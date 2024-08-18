05192dc19f180a5e38fe327111b9d5c8cf7f8e2b
Bellorico323 <mabo.orico@gmail.com>
2024-08-17 21:56:23 -0300

    feat: sign-up-form action

--- apps/web/src/app/auth/sign-up/actions.ts
+++ apps/web/src/app/auth/sign-up/actions.ts
@@ -1,31 +1,62 @@
 'use server'
 
+import { HTTPError } from 'ky'
 import { z } from 'zod'
+
+import { signUp } from '@/http/sign-up'
 
 const signUpSchema = z
   .object({
     name: z.string().refine((value) => value.split(' ').length > 1, {
-      message: 'Por favor, insira seu nome completo.',
+      message: 'Please, enter your full name.',
     }),
     email: z
       .string()
-      .email({ message: 'Por favor, insira um endereço de e-mail válido.' }),
+      .email({ message: 'Please, provide a valid e-mail address.' }),
     password: z
       .string()
-      .min(6, { message: 'A senha tem que ter no mínimo 6 caracteres' }),
+      .min(6, { message: 'Password should have at least 6 characters' }),
     password_confirmation: z
       .string()
       .min(6, { message: 'Password should have at least 6 characters' }),
   })
   .refine((data) => data.password === data.password_confirmation, {
-    message: 'Confirmação de senha incorreta.',
+    message: 'Password confirmation does not match.',
     path: ['password_confirmation'],
   })
 
 export async function signUpAction(data: FormData) {
-  console.log(data)
+  const result = signUpSchema.safeParse(Object.fromEntries(data))
 
-  const formData = signUpSchema.safeParse(Object.fromEntries(data))
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
 
-  return 'success'
+    return { success: false, message: null, errors }
+  }
+
+  const { name, email, password } = result.data
+
+  try {
+    await signUp({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json<{ message: string }>()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
--- apps/web/src/app/auth/sign-up/sign-up-form.tsx
+++ apps/web/src/app/auth/sign-up/sign-up-form.tsx
@@ -1,22 +1,48 @@
 'use client'
 
+import { Loader2 } from 'lucide-react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useFormState } from '@/hooks/use-form-state'
+
+import { signUpAction } from './actions'
 
 export function SignUpForm() {
+  const router = useRouter()
+
+  const [{ errors }, handleSubmit, isPending] = useFormState(
+    signUpAction,
+    () => {
+      router.push('/auth/sign-in')
+    },
+  )
+
   return (
-    <form action="" className="w-full space-y-2">
+    <form onSubmit={handleSubmit} className="w-full space-y-2">
       <div className="space-y-1">
         <Label htmlFor="name">Nome Completo</Label>
         <Input placeholder="Digite seu nome completo" id="name" name="name" />
+
+        {errors?.name && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.name[0]}
+          </p>
+        )}
       </div>
 
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input placeholder="Digite seu e-mail" id="email" name="email" />
+
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
       </div>
 
       <div className="space-y-1">
@@ -27,6 +53,12 @@ export function SignUpForm() {
           id="password"
           name="password"
         />
+
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
       </div>
 
       <div className="space-y-1">
@@ -37,10 +69,20 @@ export function SignUpForm() {
           id="password_confirmation"
           name="password_confirmation"
         />
+
+        {errors?.password_confirmation && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password_confirmation[0]}
+          </p>
+        )}
       </div>
 
-      <Button className="w-full" variant="primary">
-        Se cadastrar
+      <Button className="w-full" variant="primary" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size-4 animate-spin" />
+        ) : (
+          'Se cadastrar'
+        )}
       </Button>
 
       <Button variant="link" className="w-full" asChild size="sm">
